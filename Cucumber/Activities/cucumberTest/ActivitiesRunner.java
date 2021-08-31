f6176d56700f2bd41cc668b4036cb23f53d713b3
DikshaRai1 <87966923+DikshaRai1@users.noreply.github.com>
2021-08-31 18:44:06 +0530

    Added Cucumber activities

---
+++ Cucumber/Activities/cucumberTest/ActivitiesRunner.java
@@ -0,0 +1,13 @@
+package cucumberTest;
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "Features",
+    glue = {"stepDefinitions"},
+    tags = "@SimpleAlert",
+    plugin = {"pretty"},
+    monochrome = true
+)
+
+public class ActivitiesRunner {
+    //This is intentionally blank
+}
---
+++ Cucumber/Activities/cucumberTest/Demo.java
@@ -0,0 +1,24 @@
+package cucumberTest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class Demo{
+	WebDriver driver = new FirefoxDriver();
+	@Given("^user is on the Amazon website$")
+	public void user_is_on_Home_Page() throws Throwable {
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.get("http://www.amazon.in");
+	}
+
+	@When("^user searches a product$")
+	public void user_searches_a_product() throws Throwable {
+		driver.findElement(By.xpath(".//*[@id='account']/a")).click();
+	}
+}
